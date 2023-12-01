68e13a7146b9777ae1403f108d947e2836764833
Denis <denisj1688@gmail.com>
2023-12-01 10:26:37 +0100

    simplifies

--- lib/aoc2023.ex
+++ lib/aoc2023.ex
@@ -1,11 +1,11 @@
 defmodule Aoc2023DayOne do
   def get_input do
-    session = System.get_env("AOC_SESSION")
-    url = "https://adventofcode.com/2023/day/1/input"
-
-    HTTPoison.get!(url, [{"cookie", "session=#{session}"}]) 
-      |> Map.get(:body)
-      |> String.split("\n") 
-      |> Enum.each(&IO.puts/1)
+    HTTPoison.get!(
+      "https://adventofcode.com/2023/day/1/input",
+      [{"cookie", "session=#{System.get_env("AOC_SESSION")}"}]
+    )
+    |> Map.get(:body)
+    |> String.split("\n")
+    |> Enum.each(&IO.puts/1)
   end
 end
