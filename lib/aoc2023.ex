d9e72bfb141f4f9225b015ec6b01b4173aef89bd
Denis <denisj1688@gmail.com>
2023-12-01 18:39:21 +0100

    put some code down

--- lib/aoc2023.ex
+++ lib/aoc2023.ex
@@ -1,11 +1,26 @@
-defmodule Aoc2023DayOne do
-  def get_input do
+defmodule Aoc2023 do
+  def get_input_for_day(day) do
     HTTPoison.get!(
-      "https://adventofcode.com/2023/day/1/input",
+      "https://adventofcode.com/2023/day/#{day}/input",
       [{"cookie", "session=#{System.get_env("AOC_SESSION")}"}]
     )
     |> Map.get(:body)
     |> String.split("\n")
-    |> Enum.each(&IO.puts/1)
+    |> Enum.map(&Regex.replace(~r/[^0-9]/, &1, ""))
+    |> Enum.reject(&(&1 == ""))
+    |> Enum.map(
+      &if String.length(&1) > 1 do
+        String.to_integer("#{String.at(&1, 0) || ""}#{String.at(&1, -1) || ""}")
+      else
+        String.to_integer(&1)
+      end
+    )
+    |> Enum.sum()
+  end
+
+  defmodule Day1 do
+    def part1 do
+      Aoc2023.get_input_for_day(1)
+    end
   end
 end
