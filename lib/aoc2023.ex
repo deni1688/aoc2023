a6964861731ecf4c0d2b80888e197094b60a2c56
Denis Junuzovic <denisj1688@gmail.com>
2023-12-04 11:25:55 +0100

    day 1 part 2 - should work but it does not

--- lib/aoc2023.ex
+++ lib/aoc2023.ex
@@ -14,7 +14,32 @@ defmodule Aoc2023 do
       |> Enum.map(&Regex.replace(~r/\D/, &1, ""))
       |> Enum.reject(&(&1 == ""))
       |> Enum.map(fn x -> String.at(x, 0) <> String.at(x, -1) end)
-      |> Enum.reject(&(String.length(&1) == 1))
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.sum()
+    end
+
+    def part2 do
+      Aoc2023.get_input_for_day(1)
+      |> String.split("\n")
+      |> Enum.map(fn x ->
+        Regex.scan(~r/\d+|one|two|three|four|five|six|seven|eight|nine/, x)
+        |> List.flatten
+        |> Enum.map(&case &1 do
+            "one" -> "1"
+            "two" -> "2"
+            "three" -> "3"
+            "four" -> "4"
+            "five" -> "5"
+            "six" -> "6"
+            "seven" -> "7"
+            "eight" -> "8"
+            "nine" -> "9"
+            _ -> &1
+            end)
+        |> Enum.join
+        end)
+      |> Enum.reject(&(&1 == ""))
+      |> Enum.map(fn x -> String.at(x, 0) <> String.at(x, -1) end)
       |> Enum.map(&String.to_integer/1)
       |> Enum.sum()
     end
